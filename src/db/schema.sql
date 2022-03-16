244c32861cef4079ffc2206703c15af9237ad874
Tayyib Mahmood <Tayyibmahmood2001@outlook.com>
2022-03-16 20:59:36 +0000

    remove casing

--- src/db/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS tech_blog_db;
-
-CREATE DATABASE tech_blog_db;
-
-USE tech_blog_db;
--- src/routes/api/comments.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { createComment } = require("../../controllers/api/comments");
-
-const router = Router();
-
-router.post("/", createComment);
-
-module.exports = router;
